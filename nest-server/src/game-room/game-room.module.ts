a01e4a7a796f8d3cdff0354a034acd3be8ae44d3
박태훈 <xun415@naver.com>
2024-01-10 12:37:42 +0900

    🏗️: npm -> yarn berry

--- nest-server/src/game-room/game-room.module.ts
+++ nest-server/src/game-room/game-room.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GameRoomController } from './game-room.controller';
 import { GameRoomService } from './game-room.service';
 import {GameRoomGateway} from "./game-room.gateway";
+import {UserService} from "../user/user.service";
 
 @Module({
   controllers: [GameRoomController],
-  providers: [GameRoomService, GameRoomGateway]
+  providers: [GameRoomService, GameRoomGateway, UserService]
 })
 export class GameRoomModule {}
